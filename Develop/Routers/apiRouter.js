82c50a8eee495986c91c44971cf9c8b9fbe40520
Erik De Luna <codingerik1.gmail.com>
2020-07-06 05:17:04 -0500

    setting up routers for each file

---
+++ Develop/Routers/apiRouter.js
@@ -0,0 +1,20 @@
+// // Adding fs module to help access db.json file
+// var fs = require("fs");
+// // Adding path module to help navigate directories
+// var path = require("path");
+// // Creating variable to hold db.json path
+// const dbDir = path.resolve(__dirname, "../db");
+
+
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const apiRouter = express.Router(); 
+
+
+
+
+
+
+module.exports = apiRouter; 
---
+++ Develop/Routers/htmlRouter.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const apiRouter = express.Router(); 
+
+
+
+
+
+
+module.exports = apiRouter; 
