f37c8d7bd4c8e0d5ebaf360b25c61a18b4a24f6d
Erik De Luna <codingerik1.gmail.com>
2020-07-06 05:56:57 -0500

    recommiting the json file

--- Develop/Routers/apiRouter.js
+++ Develop/Routers/apiRouter.js
@@ -14,15 +14,27 @@ const apiRouter = express.Router();
 
 
 apiRouter.get(`/notes`, (req, res, next) => {
-    fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', (err, data) => {
+    fs.readFile(path.resolve(__dirname, "../db/db.json"), 'utf8', (err, data) => {
         if (err) console.log(err.message); 
         res.json(data);
     })
 })
 
+// Should receive a new note to save on the request body
+// add it to the `db.json` file, and then return the new note to the client.
+
 
 apiRouter.post(`/notes`, (req, res, next) => {
+    let userNote = req.body; 
+    // this is the note that people can read and such 
+    // console.log(userNote);
+    fs.writeFile('message.txt', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
 
+    
+    res.sendStatus(200); 
 })
 
 
