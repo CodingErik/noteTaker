724055af71c03547d48c84341df516f37a2e6358
Erik De Luna <codingerik1.gmail.com>
2020-07-13 04:11:56 -0500

    deleting morgan

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // morgan gives me basic info about my request
-const morgan = require('morgan'); 
+// const morgan = require('morgan'); 
 const _ = require('lodash'); 
 
 
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.static('Develop/public'));  // maybe assets  // what happens when we take this off let's try it
 
 // setting up a small middleWare for testing
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 // ****************************************
 
 
